9a220d6defccf7ddf6b7a0376761dd92d3755b51
Félix <the2deux@gmail.com>
2017-10-25 06:03:55 -0400

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+//@prepros-prepend theme/jquery.min.js
+//@prepros-prepend theme/vendor/bootstrap.js
+//@prepros-prepend theme/vendor/owl.carousel.js
+//@prepros-prepend theme/jquery.stellar.min.js
+//@prepros-prepend theme/wow.min.js
+//@prepros-prepend theme/gmap3.min.js
+//@prepros-prepend theme/form-contact.js
+//@prepros-prepend theme/scripts.js
